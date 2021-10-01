ea60e3ada7991c8c515eed85fe98990fe195d534
Jerry Y. Chen <chen@jyny.dev>
2021-10-01 21:17:02 +0800

    refactor: abstract operation make CountPageTotal more test able

--- paginator.go
+++ paginator.go
@@ -35,9 +35,7 @@ func New(page Page, orders []Order, filter map[string]string) *Paginator {
 
 // GenGormTransaction generate GORMv2 sql Transaction (gorm.DB) for paging
 func (pgntr Paginator) GenGormTransaction(tx *gorm.DB) *gorm.DB {
-	tx = pgntr.offset(tx)
-	tx = pgntr.limit(tx)
-	tx = pgntr.orderBy(tx)
+	tx = pgntr.GenPgntrStmt(tx)
 	tx = pgntr.where(tx)
 
 	return tx
@@ -45,14 +43,39 @@ func (pgntr Paginator) GenGormTransaction(tx *gorm.DB) *gorm.DB {
 
 // CountPageTotal is setter of Paginator.Page.Total by counting total page number
 func (pgntr *Paginator) CountPageTotal(tx *gorm.DB) error {
-	var count int64
 	// remove offset, limit and order by before count
-	delete(tx.Statement.Clauses, "ORDER BY")
-	tx.Offset(-1).Limit(-1).Count(&count)
-	pgntr.Page.Total = int(math.Ceil(float64(count) / float64(pgntr.Page.Size)))
+	tx = pgntr.DelPgntrStmt(tx)
+
+	var recordCount int64
+	if err := tx.Count(&recordCount).Error; err != nil {
+		return err
+	}
+
+	// set PageTotal by counting total page number
+	pgntr.Page.Total = pgntr.countPageTotal(int(recordCount))
+
 	// limit PageNumber <= PageTotal
-	pgntr.LimitPageTotal()
+	pgntr.limitPageTotal()
+
 	return nil
+}
+
+// GenPgntrStmt add statements for paging
+func (pgntr Paginator) GenPgntrStmt(tx *gorm.DB) *gorm.DB {
+	tx = pgntr.offset(tx)
+	tx = pgntr.limit(tx)
+	tx = pgntr.orderBy(tx)
+
+	return tx
+}
+
+// DelPgntrStmt remove statements for paging
+func (pgntr Paginator) DelPgntrStmt(tx *gorm.DB) *gorm.DB {
+	tx = pgntr.rmOrderBy(tx)
+	tx = pgntr.rmLimit(tx)
+	tx = pgntr.rmOffset(tx)
+
+	return tx
 }
 
 func (pgntr Paginator) where(tx *gorm.DB) *gorm.DB {
@@ -64,9 +87,17 @@ func (pgntr Paginator) offset(tx *gorm.DB) *gorm.DB {
 	return tx.Offset((pgntr.Page.Number - 1) * pgntr.Page.Size)
 }
 
+func (pgntr Paginator) rmOffset(tx *gorm.DB) *gorm.DB {
+	return tx.Offset(-1)
+}
+
 func (pgntr Paginator) limit(tx *gorm.DB) *gorm.DB {
 	// concat LIMIT SQL query statement by Paginator.Page.Size
 	return tx.Limit(pgntr.Page.Size)
+}
+
+func (pgntr Paginator) rmLimit(tx *gorm.DB) *gorm.DB {
+	return tx.Limit(-1)
 }
 
 func (pgntr Paginator) orderBy(tx *gorm.DB) *gorm.DB {
@@ -75,6 +106,20 @@ func (pgntr Paginator) orderBy(tx *gorm.DB) *gorm.DB {
 		tx = tx.Order(clause.OrderByColumn{Column: clause.Column{Name: order.Column}, Desc: order.Direction == SortDESC})
 	}
 	return tx
+}
+
+func (pgntr Paginator) rmOrderBy(tx *gorm.DB) *gorm.DB {
+	delete(tx.Statement.Clauses, "ORDER BY")
+	return tx
+}
+
+func (pgntr Paginator) countPageTotal(recordCount int) (pageTotal int) {
+	// page total start with 1
+	if recordCount == 0 || pgntr.Page.Size == 0 {
+		return 1
+	}
+
+	return int(math.Ceil(float64(recordCount) / float64(pgntr.Page.Size)))
 }
 
 func (pgntr *Paginator) limitMinPageNumber(minPageNumber int) {
@@ -91,11 +136,12 @@ func (pgntr *Paginator) limitMinPageSize(minPageSize int) {
 	}
 }
 
-func (pgntr *Paginator) LimitPageTotal() {
+func (pgntr *Paginator) limitPageTotal() {
 	// set page total default to 1
 	if pgntr.Page.Total == 0 {
 		pgntr.Page.Total = 1
 	}
+
 	// limit PageNumber <= PageTotal
 	if pgntr.Page.Number > pgntr.Page.Total {
 		pgntr.Page.Number = pgntr.Page.Total
